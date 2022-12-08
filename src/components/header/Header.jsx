cfd4c8495da7f22ba0d658dac518de277b41f19a
Curran Duke <cdsonics@yahoo.com>
2022-12-07 19:54:33 -0500

    finished structure for headersocials

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,15 +1,23 @@
 import React from 'react'
 import './header.css'
 import CTA from './CTA'
+import ME from '../../assets/images/Placeholder.png'
+import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
   return (
     <header>
-      <div className="container header__container"></div>
+      <div className="container header__container">
         <h5>Hello I'm</h5>
         <h1> Curran Duke</h1>
         <h5 className='text-light'> Fullstack Developer</h5>
         <CTA></CTA>
+        <HeaderSocials/>
+        <div className='me'>
+          <img src={ME} alt='me'></img>
+        </div>
+        <a href='#contact' className='scroll__down'>Skip to bottom</a>
+      </div>
     </header>
   )
 }
