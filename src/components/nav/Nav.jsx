dd0a39025fad2e5e3a3172f16a4cbbd842e21786
Curran Duke <cdsonics@yahoo.com>
2022-12-08 19:24:04 -0500

    finished nav bar

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import './nav.css'
+import {FaHome} from 'react-icons/fa'
+import {BsFillPersonFill} from 'react-icons/bs'
+import {FaMedal} from 'react-icons/fa'
+import {MdHomeRepairService} from 'react-icons/md'
+import {BsChatTextFill} from 'react-icons/bs'
+import {useState} from 'react'
 
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState('#')
   return (
-    <div>Nav</div>
+    <nav>
+      <a href='#' onClick={() =>setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><FaHome/></a>
+      <a href='#about' onClick={() =>setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><BsFillPersonFill/></a>
+      <a href='#experience' onClick={() =>setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><FaMedal/></a>
+      <a href='#services' onClick={() =>setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}><MdHomeRepairService/></a>
+      <a href='#contact' onClick={() =>setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><BsChatTextFill/></a>
+    </nav>
   )
 }
 
